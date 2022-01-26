911e71a1206743a9e8b084691a3887cfa70c7da4
illupovero <davideluporini1996@gmail.com>
2022-01-26 16:19:57 +0100

    ended exsercise

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
  */
+
+
+console.log('Vue ok', Vue);
+
+const root = new Vue({
+    el: '#root',
+    data: {
+        message: 'Image generator',
+        subtitle: 'search your prefer images',
+        img: '',
+    }
+})
